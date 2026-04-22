2d91a252118f1e185a78cbb670b426d4d22bca4d
Melanie <git@melanie-busse.de>
2026-04-22 10:07:05 +0200

    fix: vercel development insert pool

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,28 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import * as mariadb from "mariadb";
+import { PrismaTiDBCloud } from "@tidbcloud/prisma-adapter";
+import { connect } from "@tidbcloud/serverless";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prismaClientSingleton = () => {
-  const pool = mariadb.createPool({
-    uri: process.env.DATABASE_URL!,
-    ssl: {
-      rejectUnauthorized: true,
-    },
-    // Serverless Optimierung:
-    connectionLimit: 1, // In Serverless ist 1 oft besser, da jede Function ihre eigene Instanz ist
-    connectTimeout: 20000, // Mehr Zeit für den TLS-Handshake (20s)
-    acquireTimeout: 20000, // Mehr Zeit, um eine Verbindung aus dem Pool zu fischen
-    noDelay: true, // Schaltet Nagle's Algorithmus aus (schnellere Pakete)
-  } as any);
-
-  const adapter = new PrismaMariaDb(pool as any);
-
-  return new PrismaClient({
-    adapter: adapter as any,
-    log: ["error"],
-  });
+  const connection = connect({ url: process.env.DATABASE_URL });
+  const adapter = new PrismaTiDBCloud(connection as any);
+  return new PrismaClient({ adapter, log: ["error"] });
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
